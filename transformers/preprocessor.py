cbf1f87d7ccc5f67b1e14070e1f148b6c66f26dd
Humberto Yusta Gómez <humbertoyusta02@gmail.com>
2023-12-12 09:59:05 +0100

    plots-and-preprocessing

---
+++ transformers/preprocessor.py
@@ -0,0 +1,27 @@
+
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures
+
+class Preprocessor(BaseEstimator, TransformerMixin):
+    def __init__(self, add_polynomial_features=False, degree=2):
+        self.scaler = MinMaxScaler()
+        self.add_polynomial_features = add_polynomial_features
+        self.degree = degree
+        self.poly = PolynomialFeatures(degree=self.degree, include_bias=False) if self.add_polynomial_features else None
+
+    def fit(self, X, y=None):
+        self.scaler.fit(X)
+
+        if self.add_polynomial_features:
+            self.poly.fit(X)
+
+        return self
+
+    def transform(self, X):
+        X_scaled = self.scaler.transform(X)
+
+        if self.add_polynomial_features:
+            X_scaled = self.poly.transform(X_scaled)
+
+        return X_scaled
